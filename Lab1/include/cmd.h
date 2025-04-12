b9780f758204a33de88270babca04f99edf562a2
marcelo-mlv <marceloloiolaveras@gmail.com>
2025-04-11 23:45:33 -0300

    add: command parsing and argument separation

--- Lab1/include/cmd.h
+++ Lab1/include/cmd.h
@@ -1,7 +1,8 @@
 #include <malloc.h>
+#include <string.h>
 
 char* get_cmd() {
-    char *cmd = (char *)malloc(100);
+    char *cmd = (char *)malloc(100*sizeof(char));
     if (cmd == NULL) {
         fprintf(stderr, "Memory allocation failed\n");
         return NULL;
@@ -11,3 +12,31 @@ char* get_cmd() {
     return cmd;
 }
 
+char** separate_args(char *cmd) {
+    char **args = (char **)malloc(10*sizeof(char*));
+    if (args == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return NULL;
+    }
+    for(int i = 0; cmd[i] != '\0'; i++) {
+        if(cmd[i] == ' ') {
+            cmd[i] = '\0';
+            args[i] = &cmd[i+1];
+        } else {
+            args[i] = &cmd[i];
+        }
+    }
+    return args;
+}
+
+void parse_cmd(char *cmd) {
+    char** args = separate_args(cmd);
+    if(strcmp(args[0], "exit") == 0) {
+        printf("Exiting shell...\n");
+    }
+    else printf("Command not found: \"%s\"\n", args[0]);
+    
+    free(args);
+    free(cmd);
+    return;
+}
--- Lab1/src/main.c
+++ Lab1/src/main.c
@@ -6,7 +6,10 @@ int main() {
     printf("Welcome to the miniature-shell.\n");
     
     printf("cmd> ");
+    
     char *cmd = get_cmd();
+    
+    parse_cmd(cmd);
 
     return 0;
 }
